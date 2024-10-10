c785a811107a51286a90ade81d247ee4fb0a6de1
hartmanm <16545449+hartmanm@users.noreply.github.com>
2024-10-10 22:48:04 +0000

    initial typed integration

---
+++ config_link_page.js
@@ -0,0 +1,35 @@
+const config_copyright=`
+Copyright (c) 2019 Michael Neill Hartman. All rights reserved.
+mnh_license@proton.me
+https://github.com/hartmanm
+`
+// the speed characters are output at
+const typing_speed=14
+// seconds to hold message before clearing
+const time_to_hold_before_clearing=0
+// larger is faster accelerates the speed characters are output at
+// no smaller than 1; 1 is no acceleration
+const accelerator=1.5
+// frequency at which output is updated in milliseconds
+const update_timeout_frequency=20
+
+const input_text=`
+    My name is Michael Neill Hartman
+
+    I create solutions with software.
+    
+    I live in New Hampshire
+    
+    I'm an experimental innovator who finds creative solutions to difficult problems.
+    
+    
+    Whenever I attempt a technical task I succeed. 
+    
+    I have lots of extra-domain knowledge. 
+    
+    I am extremely eclectic in my ideas and opinions. 
+    
+    I am always looking to be proven wrong, learn new things, or be afforded a fresh perspective.
+    
+    I would rather be challenged, than comfortable. 
+`
---
+++ lib_link_page.js
@@ -0,0 +1,47 @@
+const copyright=`
+Copyright (c) 2021 Michael Neill Hartman. All rights reserved.
+mnh_license@proton.me
+https://github.com/hartmanm
+`
+const info ="A vanilla js DOM metaprogramming implementation";
+
+var intro = document.getElementsByClassName("intro");
+var zero = document.getElementsByClassName("zero");
+var zero_one = document.getElementsByClassName("zero_one");
+var one = document.getElementsByClassName("one");
+var two = document.getElementsByClassName("two");
+var three = document.getElementsByClassName("three");
+var footer = document.getElementsByClassName("footer");
+function lamda_link(innerhtml,id,url,base_element)
+{
+var ddiv = document.createElement("lamda_link");
+ddiv.innerHTML = innerhtml;
+ddiv.id = id;
+var first=base_element[0];
+first.appendChild(ddiv);
+function execute_z(element,url){window.open(url);}
+var lamdalink = document.getElementById(id);
+lamdalink.addEventListener("click", function(e){var target = e.target || e.srcElement; execute_z(target,url)}, false);
+}
+// div intro
+lamda_link('<p>hartmanm/mnh_license</p>','bsdfgg','https://github.com/hartmanm/mnh_license/tree/main',intro);
+// div zero
+lamda_link('<p>hartmanm/typed</p>','1ert','https://github.com/hartmanm/typed',zero);
+//lamda_link('<img src="xyz.png" style="width:50%;height:50%;"/>','1dfg','title_url',zero);
+// div zero_one
+lamda_link('<p>hartmanm/overlord_nv</p>','1esdfgrt','https://github.com/hartmanm/overlord_nv',zero_one);
+//lamda_link('<img src="xyz.png" style="width:50%;height:50%;"/>','1dsdffg','title_url',zero_one);
+// div one
+lamda_link('<p>hartmanm/oros_nv</p>','1','https://github.com/hartmanm/oros_nv',one);
+//lamda_link('<img src="xyz.png"/>','1','title_url',one);
+//lamda_link('<img src="xyz.png" style="width:50%;height:50%;"/>','2','title_url',one);
+// div two
+lamda_link('<p>hartmanm/22mines</p>','22','https://github.com/hartmanm/22mines',two);
+//lamda_link('<img src="xyz.png"/>','3','title_url',two);
+// div three
+lamda_link('<p>hartmanm/hs_server</p>','44','https://github.com/hartmanm/hs_server',three);
+//lamda_link('<img src="xyz.png" style="width:50%;height:50%;"/>','4','title_url',three);
+//lamda_link('<img src="xyz.png" style="width:50%;height:50%;"/>','4asdf44','title_url',three);
+//lamda_link('<img src="xyz.png" style="width:50%;height:50%;"/>','444','title_url',three);
+// div footer
+lamda_link('<img src="hartmanm_avatar.png"/>','12345','https://github.com/hartmanm',footer);
