737514d4fb44b8194674ea37eb1d07aac3fc89c2
hartmanm <16545449+hartmanm@users.noreply.github.com>
2024-10-11 19:38:27 +0000

    changed index

--- lib_link_page.js
+++ lib_link_page.js
@@ -23,6 +23,7 @@ function execute_z(element,url){window.open(url);}
 let lamdalink = document.getElementById(id);
 lamdalink.addEventListener("click", function(e){let target = e.target || e.srcElement; execute_z(target,url)}, false);
 }
+lamda_link('<p>license</p>','1','https://hartmanm.github.io/mnh_license/mnh_license.html',one);
 lamda_link('<p>hartmanm/mnh_license</p>','1','https://github.com/hartmanm/mnh_license/tree/main?tab=License-1-ov-file',one);
 lamda_link('<p>hartmanm/typed</p>','2','https://github.com/hartmanm/typed?tab=License-1-ov-file',two);
 lamda_link('<p>hartmanm/hs_server</p>','8','https://github.com/hartmanm/hs_server',three);
@@ -31,5 +32,4 @@ lamda_link('<p>hartmanm/oros_nv</p>','4','https://github.com/hartmanm/oros_nv?ta
 lamda_link('<p>hartmanm/overlord_nv</p>','5','https://github.com/hartmanm/overlord_nv?tab=License-1-ov-file',six);
 lamda_link('<p>hartmanm/methods</p>','6','https://github.com/hartmanm/methods?tab=License-1-ov-file',seven);
 lamda_link('<p>hartmanm/22mines</p>','7','https://github.com/hartmanm/22mines?tab=License-1-ov-file',eight);
-
 lamda_link('<img src="hartmanm_avatar.png" style="width:20%;height:20%;"/>','zzzz','https://github.com/hartmanm',footer);
