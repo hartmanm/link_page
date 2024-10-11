30b4eb3869d493aff4c265ab0d66e68f1c2eefe8
hartmanm <16545449+hartmanm@users.noreply.github.com>
2024-10-11 03:17:24 +0000

    changed ll index

--- lib_link_page.js
+++ lib_link_page.js
@@ -23,15 +23,15 @@ function execute_z(element,url){window.open(url);}
 let lamdalink = document.getElementById(id);
 lamdalink.addEventListener("click", function(e){let target = e.target || e.srcElement; execute_z(target,url)}, false);
 }
-lamda_link('<p>hartmanm/mnh_license</p>','ai','https://github.com/hartmanm/mnh_license/tree/main?tab=License-1-ov-file',intro);
-lamda_link('<p>hartmanm/typed</p>','a0','https://github.com/hartmanm/typed?tab=License-1-ov-file',zero);
+lamda_link('<p>hartmanm/mnh_license</p>','1','https://github.com/hartmanm/mnh_license/tree/main?tab=License-1-ov-file',intro);
+lamda_link('<p>hartmanm/typed</p>','2','https://github.com/hartmanm/typed?tab=License-1-ov-file',zero);
 //lamda_link('<img src="xyz.png" style="width:50%;height:50%;"/>','1dfg','title_url',zero);
-lamda_link('<p>hartmanm/d3_clock</p>','a01','https://github.com/hartmanm/d3_clock?tab=License-1-ov-file',zero_one);
-lamda_link('<p>hartmanm/overlord_nv</p>','1esdfgrt','https://github.com/hartmanm/overlord_nv?tab=License-1-ov-file',one);
-lamda_link('<p>hartmanm/oros_nv</p>','a2','https://github.com/hartmanm/oros_nv?tab=License-1-ov-file',two);
-lamda_link('<p>hartmanm/22mines</p>','a3','https://github.com/hartmanm/22mines?tab=License-1-ov-file',three);
+lamda_link('<p>hartmanm/d3_clock</p>','3','https://github.com/hartmanm/d3_clock?tab=License-1-ov-file',zero_one);
+lamda_link('<p>hartmanm/overlord_nv</p>','4','https://github.com/hartmanm/overlord_nv?tab=License-1-ov-file',one);
+lamda_link('<p>hartmanm/oros_nv</p>','5','https://github.com/hartmanm/oros_nv?tab=License-1-ov-file',two);
+lamda_link('<p>hartmanm/22mines</p>','6','https://github.com/hartmanm/22mines?tab=License-1-ov-file',three);
 //lamda_link('<img src="xyz.png" style="width:50%;height:50%;"/>','4','title_url',three);
 //lamda_link('<img src="xyz.png" style="width:50%;height:50%;"/>','4asdf44','title_url',three);
 //lamda_link('<img src="xyz.png" style="width:50%;height:50%;"/>','444','title_url',three);
-lamda_link('<p>hartmanm/hs_server</p>','a4','https://github.com/hartmanm/hs_server',four);
-lamda_link('<img src="hartmanm_avatar.png"/>','afzzz','https://github.com/hartmanm',footer);
+lamda_link('<p>hartmanm/hs_server</p>','7','https://github.com/hartmanm/hs_server',four);
+lamda_link('<img src="hartmanm_avatar.png"/>','zzzz','https://github.com/hartmanm',footer);
