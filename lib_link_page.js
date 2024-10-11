70bc9094ef14c4f3bd24be8b8a96f23b339cd008
hartmanm <16545449+hartmanm@users.noreply.github.com>
2024-10-11 14:53:00 -0700

    changed img size

--- lib_link_page.js
+++ lib_link_page.js
@@ -32,4 +32,4 @@ lamda_link('<p>hartmanm/oros_nv</p>','4','https://github.com/hartmanm/oros_nv?ta
 lamda_link('<p>hartmanm/overlord_nv</p>','5','https://github.com/hartmanm/overlord_nv?tab=License-1-ov-file',six);
 lamda_link('<p>hartmanm/methods</p>','6','https://github.com/hartmanm/methods?tab=License-1-ov-file',seven);
 lamda_link('<p>hartmanm/22mines</p>','7','https://github.com/hartmanm/22mines?tab=License-1-ov-file',eight);
-lamda_link('<img src="hartmanm_avatar.png" style="width:20%;height:20%;"/>','zzzz','https://github.com/hartmanm',footer);
+lamda_link('<img src="hartmanm_avatar.png" style="width:15%;height:15%;"/>','zzzz','https://github.com/hartmanm',footer);
