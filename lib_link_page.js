6eb4d653f669e984e5bfad53cddaabe5152a9154
hartmanm <16545449+hartmanm@users.noreply.github.com>
2024-10-12 13:59:06 +0000

    added more ll

--- lib_link_page.js
+++ lib_link_page.js
@@ -11,7 +11,9 @@ let five = document.getElementsByClassName("five");
 let six = document.getElementsByClassName("six");
 let seven = document.getElementsByClassName("seven");
 let eight = document.getElementsByClassName("eight");
-let footer = document.getElementsByClassName("footer");
+let nine = document.getElementsByClassName("nine");
+let ten = document.getElementsByClassName("ten");
+let eleven = document.getElementsByClassName("eleven");
 function lamda_link(innerhtml,id,url,base_element)
 {
 let ddiv = document.createElement("lamda_link");
@@ -32,4 +34,6 @@ lamda_link('<p>hartmanm/oros_nv</p>','4','https://github.com/hartmanm/oros_nv?ta
 lamda_link('<p>hartmanm/overlord_nv</p>','5','https://github.com/hartmanm/overlord_nv?tab=License-1-ov-file',six);
 lamda_link('<p>hartmanm/methods</p>','6','https://github.com/hartmanm/methods?tab=License-1-ov-file',seven);
 lamda_link('<p>hartmanm/22mines</p>','7','https://github.com/hartmanm/22mines?tab=License-1-ov-file',eight);
-lamda_link('<img src="hartmanm_avatar.png" style="width:15%;height:15%;"/>','zzzz','https://github.com/hartmanm',footer);
+lamda_link('<p>hartmanm/mining_electric_cost_calculator</p>','9','https://github.com/hartmanm/mining_electric_cost_calculator',nine);
+lamda_link('<p>placeholder</p>','10','placeholder',ten);
+lamda_link('<img src="hartmanm_avatar.png" style="width:15%;height:15%;"/>','11','https://github.com/hartmanm',eleven);
