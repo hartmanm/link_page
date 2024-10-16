245cc9c37be4e84dd158551270d4085d7814f4a4
hartmanm <16545449+hartmanm@users.noreply.github.com>
2024-10-15 19:20:31 -0700

    adding ll

--- lib_link_page.js
+++ lib_link_page.js
@@ -18,6 +18,7 @@ let twelve = document.getElementsByClassName("twelve");
 let thirteen = document.getElementsByClassName("thirteen");
 let fourteen = document.getElementsByClassName("fourteen");
 let fifteen = document.getElementsByClassName("fifteen");
+let sixteen = document.getElementsByClassName("sixteen");
 function lamda_link(innerhtml,id,url,base_element)
 {
 let ddiv = document.createElement("lamda_link");
@@ -32,16 +33,17 @@ lamdalink.addEventListener("click", function(e){let target = e.target || e.srcEl
 lamda_link('<p>license</p>','2024_1','https://hartmanm.github.io/mnh_license/mnh_license.html',one);
 lamda_link('<p>hartmanm/mnh_license</p>','2024_1_1','https://github.com/hartmanm/mnh_license/tree/main?tab=License-1-ov-file',one);
 lamda_link('<img src="hs_server.png" style="width:35%;height:35%;"/>','2023','https://github.com/hartmanm/hs_server',two);
-lamda_link('<p>hartmanm/typed</p>','2021','https://github.com/hartmanm/typed?tab=License-1-ov-file',three);
-lamda_link('<p>hartmanm/d3_clock</p>','2020_L','https://github.com/hartmanm/d3_clock?tab=License-1-ov-file',four);
-lamda_link('<p>hartmanm/d3_road_deaths</p>','2020','https://github.com/hartmanm/d3_road_deaths?tab=License-1-ov-file',five);
-lamda_link('<p>hartmanm/bash_simple_server</p>','2019_1','https://github.com/hartmanm/bash_simple_server?tab=License-1-ov-file',six);
-lamda_link('<p>hartmanm/oros_nv</p>','2018_L','https://github.com/hartmanm/oros_nv?tab=License-1-ov-file',seven);
-lamda_link('<p>hartmanm/overlord_nv</p>','5','https://github.com/hartmanm/overlord_nv?tab=License-1-ov-file',eight);
-lamda_link('<p>hartmanm/methods</p>','6','https://github.com/hartmanm/methods?tab=License-1-ov-file',nine);
-lamda_link('<p>hartmanm/ticker</p>','2018_3','https://github.com/hartmanm/ticker?tab=License-1-ov-file',ten);
-lamda_link('<p>hartmanm/mining_electric_cost_calculator</p>','2018_2','https://github.com/hartmanm/mining_electric_cost_calculator',eleven);
-lamda_link('<p>hartmanm/resume_site</p>','2018_1','https://github.com/hartmanm/resume_site?tab=License-1-ov-file',twelve);
-lamda_link('<p>hartmanm/pavo_td</p>','2017','https://github.com/hartmanm/pavo_td?tab=License-1-ov-file',thirteen);
-lamda_link('<p>hartmanm/simpleB</p>','2016','https://github.com/hartmanm/simpleB?tab=License-1-ov-file',fourteen);
-lamda_link('<img src="hartmanm_avatar.png" style="width:15%;height:15%;"/>','11','https://github.com/hartmanm',fifteen);
+lamda_link('<p>hartmanm/m11_iot_trigger_hub</p>','2022','https://github.com/hartmanm/m11_iot_trigger_hub?tab=License-1-ov-file',three);
+lamda_link('<p>hartmanm/typed</p>','2021','https://github.com/hartmanm/typed?tab=License-1-ov-file',four);
+lamda_link('<p>hartmanm/d3_clock</p>','2020_L','https://github.com/hartmanm/d3_clock?tab=License-1-ov-file',five);
+lamda_link('<p>hartmanm/d3_road_deaths</p>','2020','https://github.com/hartmanm/d3_road_deaths?tab=License-1-ov-file',six);
+lamda_link('<p>hartmanm/bash_simple_server</p>','2019_1','https://github.com/hartmanm/bash_simple_server?tab=License-1-ov-file',seven);
+lamda_link('<p>hartmanm/oros_nv</p>','2018_L','https://github.com/hartmanm/oros_nv?tab=License-1-ov-file',eight);
+lamda_link('<p>hartmanm/overlord_nv</p>','5','https://github.com/hartmanm/overlord_nv?tab=License-1-ov-file',nine);
+lamda_link('<p>hartmanm/methods</p>','6','https://github.com/hartmanm/methods?tab=License-1-ov-file',ten);
+lamda_link('<p>hartmanm/ticker</p>','2018_3','https://github.com/hartmanm/ticker?tab=License-1-ov-file',eleven);
+lamda_link('<p>hartmanm/mining_electric_cost_calculator</p>','2018_2','https://github.com/hartmanm/mining_electric_cost_calculator',twelve);
+lamda_link('<p>hartmanm/resume_site</p>','2018_1','https://github.com/hartmanm/resume_site?tab=License-1-ov-file',thirteen);
+lamda_link('<p>hartmanm/pavo_td</p>','2017','https://github.com/hartmanm/pavo_td?tab=License-1-ov-file',fourteen);
+lamda_link('<p>hartmanm/simpleB</p>','2016','https://github.com/hartmanm/simpleB?tab=License-1-ov-file',fifteen);
+lamda_link('<img src="hartmanm_avatar.png" style="width:15%;height:15%;"/>','11','https://github.com/hartmanm',sixteen);
