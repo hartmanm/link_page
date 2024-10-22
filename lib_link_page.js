87e749c93c67e2bdac612b3ccf354365a6281d3b
Michael Neill Hartman <16545449+hartmanm@users.noreply.github.com>
2024-10-22 09:51:03 -0700

    updated oros ll

--- lib_link_page.js
+++ lib_link_page.js
@@ -38,7 +38,7 @@ lamda_link('<p>hartmanm/typed</p>','2021','https://github.com/hartmanm/typed?tab
 lamda_link('<p>hartmanm/d3_clock</p>','2020_L','https://github.com/hartmanm/d3_clock?tab=License-1-ov-file',five);
 lamda_link('<p>hartmanm/d3_road_deaths</p>','2020','https://github.com/hartmanm/d3_road_deaths?tab=License-1-ov-file',six);
 lamda_link('<p>hartmanm/bash_simple_server</p>','2019_1','https://github.com/hartmanm/bash_simple_server?tab=License-1-ov-file',seven);
-lamda_link('<p>hartmanm/oros_nv</p>','2018_L','https://github.com/hartmanm/oros_nv?tab=License-1-ov-file',eight);
+lamda_link('<p>hartmanm/oros</p>','2018_L','https://github.com/hartmanm/oros?tab=License-1-ov-file',eight);
 lamda_link('<p>hartmanm/overlord_nv</p>','5','https://github.com/hartmanm/overlord_nv?tab=License-1-ov-file',nine);
 lamda_link('<p>hartmanm/methods</p>','6','https://github.com/hartmanm/methods?tab=License-1-ov-file',ten);
 lamda_link('<p>hartmanm/ticker</p>','2018_3','https://github.com/hartmanm/ticker?tab=License-1-ov-file',eleven);
